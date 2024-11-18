325a62f5d84901457b5f7b2108a6bd5a778cfaa3
Andi Angel <andi.angel@reach-u.com>
2024-11-18 09:25:59 +0200

    Remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, {useRef, useEffect } from 'react';
 import "./App.css";
 
 // Arcgis
-import Bookmarks from '@arcgis/core/widgets/Bookmarks';
-import Expand from '@arcgis/core/widgets/Expand';
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
 import TimeSlider from '@arcgis/core/widgets/TimeSlider';

891dc6ab999e74989defd30d5c4313d804ea2975
Andi Angel <andi.angel@reach-u.com>
2024-11-18 10:25:49 +0200

    Show more info on the reservoir popup

--- src/layers/reservoirLayer.js
+++ src/layers/reservoirLayer.js
@@ -4,31 +4,69 @@ import Color from '@arcgis/core/Color';
 import SimpleFillSymbol from '@arcgis/core/symbols/SimpleFillSymbol';
 import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
 import SimpleRenderer from '@arcgis/core/renderers/SimpleRenderer';
-import PopupTemplate from '@arcgis/core/PopupTemplate.js';
+import PopupTemplate from '@arcgis/core/PopupTemplate';
+import FieldsContent from '@arcgis/core/popup/content/FieldsContent';
+import FieldInfo from '@arcgis/core/popup/FieldInfo';
 
 function reservoirLayer() {
   const blob = new Blob([JSON.stringify(algarveReservoirs)], {
     type: "application/json"
   });
 
-  const lineSymbol = new SimpleLineSymbol({
-    width: 0
+  const name = new FieldInfo({
+    fieldName: "name",
+    label: "Name",
+    visible: true
   });
 
-  const fillSymbol = new SimpleFillSymbol({
-    color: new Color([0, 255, 0, 0.5]), // Green with 50% opacity
-    outline: lineSymbol
+  const opened_date = new FieldInfo({
+    fieldName: "opened_date",
+    label: "Opened date",
+    visible: true
+  });
+
+  const total_capacity = new FieldInfo({
+    fieldName: "total_capacity",
+    label: "Total capacity",
+    visible: true
+  });
+
+  const useful_capacity = new FieldInfo({
+    fieldName: "useful_capacity",
+    label: "Useful capacity",
+    visible: true
+  });
+
+  const wikipedia_article = new FieldInfo({
+    fieldName: "wikipedia_article",
+    label: "Wikipedia article",
+    visible: true
+  });
+
+  const snirh_page = new FieldInfo({
+    fieldName: "snirh_page",
+    label: "SNIRH page",
+    visible: true
+  });
+
+  const fieldsElement = new FieldsContent({
+    fieldInfos: [name, opened_date, total_capacity, useful_capacity, wikipedia_article, snirh_page]
   });
 
   const reservoirPopup = new PopupTemplate({
     title: "Reservoir",
-    content: "Name: {Name}"
+    content: [ fieldsElement ]
   });
 
   return new GeoJSONLayer({
     url: URL.createObjectURL(blob),
     renderer: new SimpleRenderer({
-      symbol: fillSymbol
+      symbol: new SimpleFillSymbol({
+        color: new Color([0, 255, 0, 0.5]),
+        outline: new SimpleLineSymbol({
+          width: 0
+        })
+      })
     }),
     popupEnabled: true,
     popupTemplate: reservoirPopup
